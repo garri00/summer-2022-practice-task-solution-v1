237993c2a296f3f950933dd18d86b3df558f8bf8
garri00 <garri1006@gmail.com>
2022-08-02 20:29:47 +0300

    Start repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module summer-2022-practice-task-solution-v1
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+type Trains []Train
+
+type Train struct {
+	TrainID            int
+	DepartureStationID int
+	ArrivalStationID   int
+	Price              float32
+	ArrivalTime        time.Time
+	DepartureTime      time.Time
+}
+
+func main() {
+	//	... запит даних від користувача
+	//result, err := FindTrains(departureStation, arrivalStation, criteria))
+	//	... обробка помилки
+	//	... друк result
+}
+
+func FindTrains(departureStation, arrivalStation, criteria string) (Trains, error) {
+	// ... код
+	return nil, nil // маєте повернути правильні значення
+}
